b20e0f07381f43669f06fa15215d1f2df3b9e8ec
markyang-_- <yangdaixing168@163.com>
2018-10-31 18:06:01 +0800

    *.: add types

---
+++ version/version.go
@@ -0,0 +1,26 @@
+package version
+
+import (
+	"fmt"
+
+	"github.com/golang/glog"
+)
+
+var (
+	// GitCommit will be set during make
+	GitCommit = "None"
+	// BuildDate will be set during make
+	BuildDate = "None"
+)
+
+// PrintVersionInfo print version info to standard output
+func PrintVersionInfo() {
+	fmt.Println("Git Commit Hash:", GitCommit)
+	fmt.Println("Build Date:", BuildDate)
+}
+
+// LogVersionInfo print version info to log
+func LogVersionInfo() {
+	glog.Infof("Git Commit Hash: %s", GitCommit)
+	glog.Infof("Build Date: %s", BuildDate)
+}
